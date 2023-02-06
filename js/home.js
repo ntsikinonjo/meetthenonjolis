31e0375d53056d22c9e1114abf72a3b4127e942e
Ntsikelelo <ntsikinonjo@gmail.com>
2023-02-06 22:36:13 +0200

    fixed link

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
 const home = {
-    template: '<div class="container-fluid g-0"><!-- XS --><div class="row d-block d-sm-none"><!-- header --><div class="col"><div class="xs-top-bar px-2 py-2 mx-auto"><h1 class="xs-top-bar-title">n&n</h1></div></div><!-- header-image --><div class="col"><img class="mobile-image" src="img/xs/home-header.svg" alt="header-image"></div></div></div>'
+    template: '<div class="container-fluid g-0"><!-- XS --><div class="row d-block d-sm-none"><!-- header --><div class="col"><div class="xs-top-bar px-2 py-2 mx-auto"><h1 class="xs-top-bar-title">n&n</h1></div></div><!-- header-image --><div class="col"><img class="mobile-image" src="img/xs/home-header.png" alt="header-image"></div></div></div>'
 }
 export default home
