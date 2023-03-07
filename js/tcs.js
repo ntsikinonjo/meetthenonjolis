14b6d31cd0a5a73a9fd5aabe63c7a54279f4a4dd
Ntsikelelo <ntsikinonjo@gmail.com>
2023-03-07 21:53:45 +0200

    House rules fixed

--- js/tcs.js
+++ js/tcs.js
@@ -1,16 +1,16 @@
 const tcs = {
 
-    template: getHtmlTemplate(),
+    template: getTcsHtmlTemplate(),
 
     mounted : function () {
 
-        mountedCallbacks()
+        mountedTcsCallbacks()
     }
 }
 export default tcs
 
 
-function getHtmlTemplate() {
+function getTcsHtmlTemplate() {
 
     let width = window.innerWidth
 
@@ -24,10 +24,10 @@ function getHtmlTemplate() {
     else if (width >= 768) return 'medium'
 
     // small
-    else if (width >= 576) return '<div class="container-fluid g-0 sm-tcs-bg"><!-- n&n --><div class="row"><div class="col sm-menu-divider"><div class="row"><div class="col-2"><!-- open button --><div id="open-btn" style="display:block"><img src="svg/sm/sm-open-btn-alt.svg" alt="menu-btn" width="48" height="48"></div><!-- close button --><div id="close-btn" style="display:none"><img src="svg/sm/sm-close-btn-alt.svg" alt="menu-btn" width="48" height="48"></div></div><div class="col-8"><div class="px-2 py-2 mx-auto"><h1 class="sm-tcs-top-title">n&n</h1></div></div><div class="col-2"><!-- menu overlay --><div id="sm-tcs-menu-overlay" style="display:none"><ul class="sm-tcs-menu-list"><!-- home --><li><router-link class="sm-tcs-menu-list-item" to="/"><h1>Home</h1><p class="sm-tcs-menu-list-item-description">All the important information is here.</p></router-link></li><!-- our story --><li class="mt-3"><router-link class="sm-tcs-menu-list-item" to="/story"><h1>Our story</h1><p class="sm-tcs-menu-list-item-description">Learn about us, how we met, how long we have been together, etc.</p></router-link></li><!-- our story --><li class="mt-3"><router-link class="sm-tcs-menu-list-item" to="/"><h1>Schedule</h1><p class="sm-tcs-menu-list-item-description">Be on time, relax and enjoy</p></router-link></li><!-- guest list --><li class="mt-3"><router-link class="sm-tcs-menu-list-item" to="/"><h1>Guest list</h1><p class="sm-tcs-menu-list-item-description">Find out if you are joining us.</p></router-link></li><!-- playlist --><li class="mt-3"><router-link class="sm-tcs-menu-list-item" to="/"><h1>Playlist</h1><p class="sm-tcs-menu-list-item-description">Get to know the playlist, so you can groove along. Listen on Spotify or YouTube Music.</p></router-link></li></ul></div></div></div></div></div><div class="mx-auto"><!-- house rules --><div class="row"><div class="col"><div class="mt-5 mx-auto pl-4 px-4"><h1 class="sm-tcs-title text-decoration-underline">House Rules</h1></div></div></div><!-- house rules subtitle --><div class="row"><div class="col"><div class="mt-2 mx-auto px-4"><h4 class="sm-tcs-subtitle">Just a few house rules to ensure that we have a good time.</h4></div></div></div><!-- general --><div class="row mt-5"><div class="col"><div class="mt-2 mx-auto px-4"><h5><strong>General</strong></h5></div></div></div><div class="row"><div class="col"><div class="mx-auto px-4"><ul class="sm-tcs-list"><li><p>Attendance is strictly by <strong>confirmed</strong> invitation only.</p></li><li><p>Should you wish to bring a plus one, it will be at your cost of a <strong>R1000.00</strong>, to be paid by <strong>15 March 2023</strong>. This amount is not refundable. Banking details are available on the home page.</p></li><li><p>A bar will be available, cash and cards are accepted.</p></li></ul></div></div></div><!-- not allowed --><div class="row"><div class="col"><div class="mt-2 mx-auto px-4"><h5><strong>Not allowed</strong></h5></div></div></div><div class="row"><div class="col"><div class="mx-auto px-4"><ul class="sm-tcs-list"><li><p>Open tabs.</p></li><li><p>Drinks from outside the venue.</p></li><li><p>Children under 18.</p></li></ul></div></div></div></div></div>'
+    else if (width >= 576) return getTcsSmTemplate()
 
     // extra small
-    else return getXsTemplate()
+    else return getTcsXsTemplate()
 }
 
 /** -------------------------------------------------------------------------------------------------------------------
@@ -41,12 +41,12 @@ var isOpen = false
 /** -------------------------------------------------------------------------------------------------------------------
  * START CALLBACKS
  */
-function mountedCallbacks() {
+function mountedTcsCallbacks() {
 
     let width = window.innerWidth
 
     // small
-    if (576 <= width && width < 768) setupMenuOverlay()
+    if (576 <= width && width < 768) setupTcsMenuOverlay()
 }
 // END XS -------------------------------------------------------------------------------------------------------------
 
@@ -56,7 +56,7 @@ function mountedCallbacks() {
 /** -------------------------------------------------------------------------------------------------------------------
  * START XS
  */
-function getXsTemplate() {
+function getTcsXsTemplate() {
 
     return '<div class="container-fluid g-0 xs-tcs-bg">' +
     '<!-- n&n --><div class="row mb-3"><div class="col xs-menu-divider"><div class="px-2 py-2 mx-auto"><h1 class="xs-tcs-top-title">n&n</h1></div></div></div><div class="mx-auto">' +
@@ -74,7 +74,7 @@ function getXsTemplate() {
 /** -------------------------------------------------------------------------------------------------------------------
  * START SM
  */
-function getSmTemplate() {
+function getTcsSmTemplate() {
 
     return '<div class="container-fluid g-0 sm-tcs-bg">' +
     '<!-- n&n --><div class="row"><div class="col-2">' +
@@ -92,14 +92,14 @@ function getSmTemplate() {
     '<!-- not allowed --><div class="row"><div class="col"><div class="mt-2 mx-auto px-4"><h5><strong>Not allowed</strong></h5></div></div></div><div class="row"><div class="col"><div class="mx-auto px-4"><ul class="sm-tcs-list"><li><p>Open tabs.</p></li><li><p>Drinks from outside the venue.</p></li><li><p>Children under 18.</p></li></ul></div></div></div></div></div>'
 }
 
-function setupMenuOverlay() {
+function setupTcsMenuOverlay() {
 
     isOpen = false
-    document.getElementById('open-btn').addEventListener('click', showMenuOverlay);
-    document.getElementById('close-btn').addEventListener('click', showMenuOverlay);
+    document.getElementById('open-btn').addEventListener('click', showTcsMenuOverlay);
+    document.getElementById('close-btn').addEventListener('click', showTcsMenuOverlay);
 }
 
-function showMenuOverlay() {
+function showTcsMenuOverlay() {
 
     if (!isOpen) {
 
